2db440b0ecf0a3ed21d12aa85f92671000090d69
Russell <russellsahoo24@gmail.com>
2026-06-05 14:09:08 +0530

    minor changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://web-banking-app-backend.onrender.com',
         changeOrigin: true,
       }
     }
